aca5cfd65e8e51e634f9b9eaf52c0ac9b94b4969
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-27 20:55:39 -0500

    rotten potatoes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // require libraries
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // middleware
@@ -9,14 +10,33 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// server connection with mongodb client
+mongoose.connect('mongodb+srv://admin:Abc123@cluster0.hyums.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+});
+
 let reviews = [
     {title: "Great Reviews", movieTitle: "Botman II"},
     {title: "Awesome Movie", movieTitle: "Titanic"}
 ];
 
 // get home route -> sending message
-app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
+app.get('/reviews/new', (req, res) => {
+    console.log(Review.find());
+    Review.find()
+        .then(reviews => {
+            res.render('reviews-index', { reviews: reviews });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
 });
 
 // define app route
