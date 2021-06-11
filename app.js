b88d8d9d3ab51c46585360a19c8a86ddd3be5f6f
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-10 22:22:11 -0500

    updateing input for password

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ handlebars: allowInsecurePrototypeAccess(Handlebars)}));
 app.set('view engine', 'handlebars');
 
 // server connection with mongodb client
-// const connectionString = `mongodb+srv://admin:Abc123@cluster0.hyums.mongodb.net/test`;
 const connectionString = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.hyums.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
